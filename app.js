f0852be20f4ef30ca9fe0dcd34f05f3fac871d16
Pavel Koryakin <unclekoin@gmail.com>
2021-06-16 13:27:59 +0300

    feat: finish project

---
+++ app.js
@@ -0,0 +1,13 @@
+const slides = document.querySelectorAll('.slide');
+
+const clearActiveClasses = () => {
+  slides.forEach((slide) => slide.classList.remove('active'));
+}
+
+slides.forEach((slide) => {
+  slide.addEventListener('click', () => {
+    clearActiveClasses();
+    slide.classList.add('active');
+  });
+})
+
